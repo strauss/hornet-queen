f2141e536228b27e7695d633b9bf0abc24410f14
Sascha Strauß <snoopy1alpha@gmail.com>
2024-11-02 16:03:03 +0100

    docs(license): Added License information
    - Introduced Apache 2.0 as license for the whole project
    - Cleaned up pom.xml and adjusted several dependency versions
    - Raised Kotlin version and auto-applied code-formattings
    - Set version to 0.1.0 for first release on jitpack

--- src/test/kotlin/de/dreamcube/hornet_queen/array/TestPrimitiveArray.kt
+++ src/test/kotlin/de/dreamcube/hornet_queen/array/TestPrimitiveArray.kt
@@ -1,3 +1,20 @@
+/*
+ * Hornet Queen
+ * Copyright (c) 2024 Sascha Strauß
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.dreamcube.hornet_queen.array
 
 import org.junit.Test
@@ -5,11 +22,14 @@ import java.util.*
 import kotlin.random.Random
 import kotlin.test.assertEquals
 
+private const val TEST_SIZE_FRACTION = 2
+
+
 class TestPrimitiveArray {
 
     @Test
     fun testPrimitiveByteArray() {
-        val size = PrimitiveByteArray.MAX_SIZE
+        val size = PrimitiveByteArray.MAX_SIZE / TEST_SIZE_FRACTION
         val testArray = PrimitiveByteArray(size, false)
         val nativeTestArray = PrimitiveByteArray(size, true)
         val referenceArray = ByteArray(size)
@@ -72,7 +92,7 @@ class TestPrimitiveArray {
 
     @Test
     fun testPrimitiveShortArray() {
-        val size = PrimitiveShortArray.MAX_SIZE
+        val size = PrimitiveShortArray.MAX_SIZE / TEST_SIZE_FRACTION
         val testArray = PrimitiveShortArray(size, false)
         val nativeTestArray = PrimitiveShortArray(size, true)
         val referenceArray = ShortArray(size)
@@ -135,7 +155,7 @@ class TestPrimitiveArray {
 
     @Test
     fun testPrimitiveCharArray() {
-        val size = PrimitiveCharArray.MAX_SIZE
+        val size = PrimitiveCharArray.MAX_SIZE / TEST_SIZE_FRACTION
         val testArray = PrimitiveCharArray(size, false)
         val nativeTestArray = PrimitiveCharArray(size, true)
         val referenceArray = CharArray(size)
@@ -198,7 +218,7 @@ class TestPrimitiveArray {
 
     @Test
     fun testPrimitiveIntArray() {
-        val size = PrimitiveIntArray.MAX_SIZE
+        val size = PrimitiveIntArray.MAX_SIZE / TEST_SIZE_FRACTION
         val testArray = PrimitiveIntArray(size, false)
         val nativeTestArray = PrimitiveIntArray(size, true)
         val referenceArray = IntArray(size)
@@ -261,7 +281,7 @@ class TestPrimitiveArray {
 
     @Test
     fun testPrimitiveLongArray() {
-        val size = PrimitiveLongArray.MAX_SIZE
+        val size = PrimitiveLongArray.MAX_SIZE / TEST_SIZE_FRACTION
         val testArray = PrimitiveLongArray(size, false)
         val nativeTestArray = PrimitiveLongArray(size, true)
         val referenceArray = LongArray(size)
@@ -324,7 +344,7 @@ class TestPrimitiveArray {
 
     @Test
     fun testPrimitiveFloatArray() {
-        val size = PrimitiveFloatArray.MAX_SIZE
+        val size = PrimitiveFloatArray.MAX_SIZE / TEST_SIZE_FRACTION
         val testArray = PrimitiveFloatArray(size, false)
         val nativeTestArray = PrimitiveFloatArray(size, true)
         val referenceArray = FloatArray(size)
@@ -387,7 +407,7 @@ class TestPrimitiveArray {
 
     @Test
     fun testPrimitiveDoubleArray() {
-        val size = PrimitiveDoubleArray.MAX_SIZE
+        val size = PrimitiveDoubleArray.MAX_SIZE / TEST_SIZE_FRACTION
         val testArray = PrimitiveDoubleArray(size, false)
         val nativeTestArray = PrimitiveDoubleArray(size, true)
         val referenceArray = DoubleArray(size)
@@ -450,7 +470,7 @@ class TestPrimitiveArray {
 
     @Test
     fun testUUIDArray() {
-        val size = UUIDArray.MAX_SIZE
+        val size = UUIDArray.MAX_SIZE / TEST_SIZE_FRACTION
         val testArray = UUIDArray(size, false)
         val nativeTestArray = UUIDArray(size, true)
         val referenceArray: Array<UUID> = Array(size) { UUID(0L, 0L) }
