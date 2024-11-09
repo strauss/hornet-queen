7a7b25e8f02795ebfdcbae27d7a2c98746999f3c
strauss <strauss@users.noreply.github.com>
2024-11-09 22:57:19 +0100

    fix(array): Fixed performance issues when copying native generic arrays
    - The underlying ByteBuffers are now copied as intended
    - PrimitiveArray now officially implements Iterable
    - Adjusted speed comparisons for arrays
    - Added speed comparisons for sets
    - Removed obsolete trial class for linked list
    - Updated README.md to reflect the performance improvements
    - Added links to README.md where applicable
    - Raised version to 0.2.0

--- src/test/kotlin/de/dreamcube/hornet_queen/array/array_trial.kt
+++ src/test/kotlin/de/dreamcube/hornet_queen/array/array_trial.kt
@@ -20,8 +20,8 @@ package de.dreamcube.hornet_queen.array
 import kotlin.random.Random
 
 fun main() {
-    val ia = PrimitiveIntArray(PrimitiveIntArray.MAX_SIZE / 2)
-    val nia = PrimitiveIntArray(PrimitiveIntArray.MAX_SIZE / 2)
+    val ia = PrimitiveIntArray(PrimitiveIntArray.MAX_SIZE / 2, native = false)
+    val nia = PrimitiveIntArray(PrimitiveIntArray.MAX_SIZE / 2, native = true)
     for (i in 0..<ia.size) {
         val nextElement = Random.nextInt()
         ia[i] = nextElement
@@ -29,12 +29,12 @@ fun main() {
     }
 
     // Time measurements
-    val expandBy = 250_000_000
+    val expandBy = PrimitiveIntArray.MAX_SIZE / 2
 
     var start = System.currentTimeMillis()
     val iac = ia.getResizedCopy(expandBy)
     var stop = System.currentTimeMillis()
-    println("Duration not native but arraycopy: ${stop - start} ms.")
+    println("Duration not native: ${stop - start} ms.")
     System.gc()
 
     Thread.sleep(1000L)
@@ -42,6 +42,6 @@ fun main() {
     start = System.currentTimeMillis()
     val niac = nia.getResizedCopy(expandBy)
     stop = System.currentTimeMillis()
-    println("Duration native but not arraycopy: ${stop - start} ms.")
+    println("Duration native: ${stop - start} ms.")
 
 }
---
+++ src/test/kotlin/de/dreamcube/hornet_queen/set/set_performance_comparison.kt
@@ -0,0 +1,140 @@
+/*
+ * Hornet Queen
+ * Copyright (c) 2024 Sascha Strauß
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.dreamcube.hornet_queen.set
+
+import de.dreamcube.hornet_queen.list.PrimitiveIntArrayList
+import de.dreamcube.hornet_queen.list.PrimitiveLongArrayList
+import java.util.*
+import kotlin.random.Random
+
+fun main() {
+    val elementCount = 10_000_000
+    uuidPerformanceTest(elementCount)
+    longPerformanceTest(elementCount)
+    intPerformanceTest(elementCount)
+}
+
+fun uuidPerformanceTest(elementCount: Int) {
+    println("UUID")
+    // generate a lot of elements
+    val elements: List<UUID> = (Array(elementCount) { UUID.randomUUID() }).toList()
+    val notElements: List<UUID> = (Array(elementCount) { UUID.randomUUID() }).toList()
+
+    println("Elements: $elementCount")
+    println()
+
+    comparisonSetTest(elements, notElements, { HashSet() }, { UUIDSet() })
+    println("-----")
+    println()
+}
+
+private fun longPerformanceTest(elementCount: Int) {
+    println("Long")
+    val elementSet: MutableSet<Long> = mutableSetOf()
+    for (i in 1..elementCount) {
+        var nextElement: Long = Random.nextLong()
+        while (elementSet.contains(nextElement)) {
+            nextElement = Random.nextLong()
+        }
+        elementSet.add(Random.nextLong())
+    }
+    val notElements: MutableList<Long> = PrimitiveLongArrayList(elementCount)
+    for (i in 1..elementCount) {
+        var nextElement: Long = Random.nextLong()
+        while (elementSet.contains(nextElement)) {
+            nextElement = Random.nextLong()
+        }
+        notElements.add(nextElement)
+    }
+    val elements: MutableList<Long> = PrimitiveLongArrayList(elementCount)
+    elements.addAll(elementSet)
+    comparisonSetTest(elements, notElements, { HashSet() }, { PrimitiveLongSet() })
+    println("-----")
+    println()
+}
+
+private fun intPerformanceTest(elementCount: Int) {
+    println("Int")
+    val elementSet: MutableSet<Int> = mutableSetOf()
+    for (i in 1..elementCount) {
+        var element = Random.nextInt()
+        while (elementSet.contains(element)) {
+            element = Random.nextInt()
+        }
+        elementSet.add(element)
+    }
+    val notElements: MutableList<Int> = PrimitiveIntArrayList(elementCount)
+    for (i in 1..elementCount) {
+        var nextElement: Int = Random.nextInt()
+        while (elementSet.contains(nextElement)) {
+            nextElement = Random.nextInt()
+        }
+        notElements.add(nextElement)
+    }
+    val elements: MutableList<Int> = PrimitiveIntArrayList(elementCount)
+    elements.addAll(elementSet)
+    comparisonSetTest(elements, notElements, { HashSet() }, { PrimitiveIntSet() }, { PrimitiveIntSetB() })
+    println("-----")
+    println()
+}
+
+private fun <T> comparisonSetTest(elements: List<T>, notElements: List<T>, vararg sets: () -> MutableSet<T>) {
+    for (setSupplier: () -> MutableSet<T> in sets) {
+        val set: MutableSet<T> = setSupplier()
+        println(set.javaClass.canonicalName)
+        setPerformanceTest(set, elements, notElements)
+    }
+}
+
+fun <T> setPerformanceTest(theSet: MutableSet<T>, elements: List<T>, notElements: List<T>) {
+    theSet.clear()
+
+    val startAdd: Long = System.currentTimeMillis()
+    for (current: T in elements) {
+        theSet.add(current)
+    }
+    val stopAdd: Long = System.currentTimeMillis()
+    println("Add: ${stopAdd - startAdd} ms")
+
+    var containsAll = true
+    val startContains: Long = System.currentTimeMillis()
+    for (current: T in elements) {
+        val contained: Boolean = theSet.contains(current)
+        containsAll = contained && containsAll
+    }
+    val stopContains: Long = System.currentTimeMillis()
+    println("Contains(+): ${stopContains - startContains} ms")
+
+    val startNotContains: Long = System.currentTimeMillis()
+    var containsNone = true
+    for (current: T in notElements) {
+        containsNone = !theSet.contains(current) && containsNone
+    }
+    val stopNotContains: Long = System.currentTimeMillis()
+    println("Contains(-): ${stopNotContains - startNotContains} ms")
+
+    val someList: MutableList<T> = ArrayList(elements.size)
+    val startIterator: Long = System.currentTimeMillis()
+    for (uuid: T in theSet) {
+        someList.add(uuid)
+    }
+    val stopIterator: Long = System.currentTimeMillis()
+    println("Iterator: ${stopIterator - startIterator} ms (${someList.size})")
+
+    println()
+}
